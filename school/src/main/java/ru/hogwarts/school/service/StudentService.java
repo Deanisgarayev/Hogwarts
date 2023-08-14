6ab700726ff3f1c6b41ec64cc728113459c8235f
Денис <garaevdenis2019@gmail.com>
2023-08-15 00:45:54 +0500

    in progress

--- school/src/main/java/ru/hogwarts/school/service/StudentService.java
+++ school/src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -3,12 +3,16 @@ package ru.hogwarts.school.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.StudentRepository;
 
 import javax.transaction.Transactional;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 @Service
@@ -50,6 +54,19 @@ public class StudentService {
 
     public void removeStudent(Long id) {
         studentRepository.deleteById(id);
+    }
+    public void uploadAvatar(Long studentID, MultipartFile file) throws IOException{
+        Student student = findStudent(studentID);
+        Path filePath = Path.of(avatarsDir, studentID + "." + getExtension(file.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+        try(InputStream is = file.getInputStream();
+            OutputStream os = Files.newOutputStream(filePath, CREAT_NEW);
+            BufferedInputStream bis = new BufferedInputStream(is, 1024);
+            BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+            ) {
+            bis.transferTo(bos);
+        }
     }
 }
 
