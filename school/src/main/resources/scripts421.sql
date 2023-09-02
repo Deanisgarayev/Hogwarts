e558fd5ac2df513b1e96259098743159194c25c2
Deanisgarayev <garaevdenis2019@gmail.com>
2023-09-02 13:09:04 +0500

    in progress

--- school/src/main/resources/scripts421.sql
+++ school/src/main/resources/scripts421.sql
@@ -2,5 +2,5 @@ alter table student add constraint age_constraint check(age>16);
 alter table student alter column name set not null;
 alter table student add constraint name_unique unique(name);
 alter table student add primary key (id); 
-alter table faculty add constraint color_unique unique(color);
+alter table faculty add constraint color_unique unique(name, color);
 alter table student alter column age set default 20;
