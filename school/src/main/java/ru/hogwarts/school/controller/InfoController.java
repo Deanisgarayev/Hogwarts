78ca3a83586e92699e1679d9ba3764f625fc9060
Deanisgarayev <garaevdenis2019@gmail.com>
2023-09-09 21:27:07 +0500

    in progress

--- school/src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ school/src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.sound.sampled.Port;
 
 @RestController
-@RequestMapping("/get port")
+@RequestMapping("/info")
 public class InfoController {
+    @Value("${service.port}")
+    private Integer port;
     @GetMapping
-    public Port port() {
-        return port();
+    public Integer getPort( ) {
+        return port;
 
     }
 }
